80a5b0eef9ec62d7f985108e6c63a3ab7215f69e
Toever1503 <Haunv1503@gmail.com>
2023-02-16 15:09:09 +0700

    update

--- src/main/java/com/example/springbootsession/SpringbootsessionApplication.java
+++ src/main/java/com/example/springbootsession/SpringbootsessionApplication.java
@@ -1,12 +1,14 @@
 package com.example.springbootsession;
 
-import jakarta.servlet.http.HttpSession;
+import jakarta.servlet.http.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.io.IOException;
 
 @SpringBootApplication
 @Controller
@@ -18,14 +20,31 @@ public class SpringbootsessionApplication {
 
     @RequestMapping("/test")
     @ResponseBody
-    public String test(HttpSession session) {
+    public String test(HttpSession session, HttpServletRequest request, HttpServletResponse res) {
         System.out.println("Session ID: " + session.getId());
+        Cookie[] cookies = request.getCookies();
         return "test";
     }
 
+    @RequestMapping("/test1")
+    @ResponseBody
+    public String test1(HttpSession session, HttpServletRequest request, HttpServletResponse res) {
+        System.out.println("Session ID: " + session.getId());
+        session.invalidate();
+        res.addCookie(new Cookie("SESSION", request.getSession(true).getId()));
+        return "test";
+    }
+
+
+
+
+
+
+
+
     @RequestMapping("/set")
     @ResponseBody
-    public String set(HttpSession session, @RequestParam String key, @RequestParam String value) {
+    public String set(HttpSession session, @RequestParam String key, @RequestParam String value, HttpServletRequest req) {
         session.setAttribute(key, value);
         return "test";
     }
@@ -34,5 +53,12 @@ public class SpringbootsessionApplication {
     @ResponseBody
     public String get(HttpSession session, @RequestParam String key) {
         return (String) session.getAttribute(key);
+    }
+
+    @RequestMapping("/redirect")
+    public void redirect(HttpServletResponse res, HttpSession session) throws IOException {
+        res.addCookie(new Cookie("SESSION", session.getId()));
+        res.addCookie(new Cookie("TESTT", session.getId()));
+        res.sendRedirect("redirect:http://192.168.1.240:8080/test");
     }
 }
---
+++ src/main/java/com/example/springbootsession/config/Interceptor.java
@@ -0,0 +1,8 @@
+package com.example.springbootsession.config;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+//@Component
+public class Interceptor implements HandlerInterceptor {
+}
